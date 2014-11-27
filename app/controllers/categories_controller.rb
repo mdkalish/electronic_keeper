cf7fe0988698882ac99dd7046eaee049fc52d615
mdkalish <mdkalish4git@gmail.com>
2014-11-27 16:13:39 +0100

    Logic for show category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,13 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
 
+  def show
+    @category = Product.where("category_name = ?", params[:category_name])
+    # binding.pry
+    respond_to :js
+  end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,18 +5,12 @@ class ProductsController < ApplicationController
     render layout: 'main'
   end
 
-  def show
-    # binding.pry
-    @product = Product.all.where(category:"salad")
-  end
-
   def ongoing_tickets
     # @ongoing_tickets = Ticket.all.where(!closed?)
   end
 
   def add_product_to_ticket
     # product = Product.find(params[:id])
-
   end
 
 end
