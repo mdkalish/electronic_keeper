fceed5d4d9a89c7ba33d8bfcebd29d297939e37a
mdkalish <mdkalish4git@gmail.com>
2014-12-30 20:38:19 +0100

    Add clock to header

--- app/assets/javascripts/custom_js.js
+++ app/assets/javascripts/custom_js.js
@@ -1,14 +1,21 @@
-// $(document).on('submit', '.product-button form.button_to',function(){
-  // console.log($('.product-button form.button_to')[0]);
-  // console.log($.get('tickets.json'));
-// });
+$(document).ready(function(){
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
 
-// $(document).ready(function(){
-//   $('.category').on('click', function(){
-//     $.getJSON('show_category/' + $(this).attr('id'));
-//   });
-// });
+    m = checkTime(m);
+    s = checkTime(s);
 
+    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+    var t = setTimeout(function(){startTime()},500);
+  }
 
-  // alert('SSIJ');
-    // $.get('/tickets.json').then(function(data){console.log(data)})
+  function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+  startTime();
+});
