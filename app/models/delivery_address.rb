21688caf85c977280cf757b4b1ada0c23b364316
mdkalish <mdkalish4git@gmail.com>
2015-01-02 17:43:24 +0100

    Add :has_many tickets to Customer and validate Customer & DeliveryAddress

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,3 +1,6 @@
 class DeliveryAddress < ActiveRecord::Base
   has_and_belongs_to_many :customers
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
 end
